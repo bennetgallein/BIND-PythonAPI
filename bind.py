222c13f931c319abe24d970058b2639c6257bf0a
bennetgallein <bennet@intranetproject.net>
2018-10-01 19:28:19 +0200

    update & delete

--- bind.py
+++ bind.py
@@ -13,6 +13,8 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+records_types = ['A', 'AAAA', 'CNAME', 'MX', 'NS', 'TXT', 'SOA']
+
 
 def parse_config(config):
     options = {}
@@ -31,8 +33,6 @@ def parse_config(config):
 def get_zone(zone_name):
 
     config = parse_config('config.ini')
-
-    record_types = ['A', 'AAAA', 'CNAME', 'MX', 'NS', 'TXT', 'SOA']
 
     records = {}
 
@@ -55,5 +55,60 @@ def get_zone(zone_name):
     return jsonify({zone_name: records})
 
 
+@app.route('/dns/record/<string:domain>', methods=['GET'])
+def get_record(domain):
+    config = parse_config('config.ini')
+
+    record = {}
+
+    for records_type in records_types:
+        try:
+            answers = dns.resolver.query(domain, records_type)
+        except dns.resolver.NoAnswer:
+            continue
+
+        record.update({records_type: [str(i) for i in answers.rrset]})
+
+    return jsonify({domain: record})
+
+
+@app.route('/dns/record/<string:domain>/<int:ttl>/<string:record_type>/<string:response>', methods=['GET', 'PUT', 'POST', 'DELETE'])
+def manage(domain, ttl, record_type, response):
+
+    zone = b'.'.join(dns.name.from_text(domain).labels[1:])
+    config = parse_config('config.ini')
+
+    if record_type not in records_types:
+        return jsonify({'error': 'not a valid record type'})
+
+    if request.method == 'PUT' or request.method == 'DELETE':
+        resolver = dns.resolver.Resolver()
+
+        resolver.nameservers = [config['nameserver']]
+        try:
+            answer = resolver.query(domain, record_type)
+        except dns.resolver.NXDOMAIN:
+            return jsonify({'error': 'domain does not exists'})
+
+    tsig = dns.tsigkeyring.from_text({config['username']: config['password']})
+    action = dns.update.Update(zone, keyring=tsig)
+    if request.method == 'DELETE':
+        action.delete(dns.name.from_text(domain).labels[0])
+    elif request.method == 'PUT' or request.method == 'POST':
+        action.replace(dns.name.from_text(domain).labels[0], ttl, str(record_type), str(response))
+
+    try:
+        response = dns.query.tcp(action, config['nameserver'])
+    #print(response)
+    except Exception as e:
+        print(e)
+        return jsonify({'error': 'DNS transaction failed'})
+
+    if response.rcode() == 0:
+        return jsonify({domain: 'DNS request successful'})
+    else:
+        return jsonify({domain: 'DNS request failed'})
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='192.168.1.127')
